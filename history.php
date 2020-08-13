a9073a925019ed3aa305c243c5b14eea99a7e24a
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-13 15:13:10 +0530

    history page added...

---
+++ history.php
@@ -0,0 +1,20 @@
+<?php include('includes/header.php');?>
+<?php include('includes/nav.php');?>
+<body>
+	 <div class="container" style="margin-top: 35px;">
+        <div class="row" >
+            <div class="col-sm-12">
+                <div class="panel-group" style="font-family: sans-serif;font-size: 17px;">
+                    <div class="panel panel-info" style="border-color: #222;">
+                        <div class="panel-body" style="padding-left: 2%;">
+                             <i class="fa fa-share"></i> <b style="font-size: 24px">Match History</b><br><br>
+                             <i class="fa fa-share"></i> Total Winnings: 0 Chips (Count: 0)<br><br>
+                             <i class="fa fa-share"></i> Total Losing: 0 Chips (Count: 0)<br><br>
+                             <i class="fa fa-share"></i> No data to display.<br><br>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+<?php include('includes/bottom_nav.php');?>
