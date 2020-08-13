778d11e8945dc25c26b358f0d9686e0f9975cfcd
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-13 15:50:29 +0530

    transaction page added........

---
+++ transaction.php
@@ -0,0 +1,23 @@
+<?php include('includes/header.php');?>
+<?php include('includes/nav.php');?>
+<body>
+	 <div class="container" style="margin-top: 35px;">
+        <div class="row" >
+            <div class="col-md-12">
+                <div class="box box-default">
+                    <div class="box-header with box-default">
+                        <h2 class="text-center">
+                            <small>Current Balance</small><br>
+                            <span class="text-success ng-binding">0</span><br>
+                            <small>Chips</small><br><br>
+                        </h2>
+                    </div>
+                    <div class="box-header with-border" style="background-color: #f5f5f5">
+                        <strong class="box-title" style="font-size: 18px;">Recents: </strong>
+                        <i class="fa fa-filter pull-right"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
+<?php include('includes/bottom_nav.php');?>
