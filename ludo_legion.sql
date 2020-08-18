410b36f2f896da558da903a3587924f218d4e7a0
cedev935 <cedev935@gmail.com>
2020-08-18 13:24:05 +0530

    updates database file uploaded

--- ludo_legion.sql
+++ ludo_legion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 15, 2020 at 02:42 PM
+-- Generation Time: Aug 18, 2020 at 09:53 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -24,6 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `open`
+--
+
+CREATE TABLE `open` (
+  `user_name` varchar(255) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `phone_no` bigint(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `running`
+--
+
+CREATE TABLE `running` (
+  `player1` varchar(255) NOT NULL,
+  `player2` varchar(255) NOT NULL,
+  `amount` bigint(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `running`
+--
+
+INSERT INTO `running` (`player1`, `player2`, `amount`) VALUES
+('Madhu', 'Naveen', 50);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -39,7 +71,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_name`, `phone_no`, `password`) VALUES
 ('Madhu', '213', 'hy'),
-('Madhu', '23', 'hy');
+('Madhu', '23', 'hy'),
+('Shabari', '8096', 'hy'),
+('Naveen', '9505', 'hy');
 
 --
 -- Indexes for dumped tables
