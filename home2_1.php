983b489b15996bc2687a3c8ed59778ac0a579f71
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-19 17:51:12 +0530

    updated home2_1 page
    room code page updated.................

--- home2_1.php
+++ home2_1.php
@@ -1,23 +1,40 @@
+<?php include('includes/server.php');?>
+<?php 
+     //$ludo_id=$_POST['postid'];
+     $user=$_SESSION['user_name'];
+     $us="SELECT user_name from users where user_name='$user'";
+     $us1=mysqli_query($db,$us);
+     $pl="SELECT player1 from running where player1='$user'";
+     $pl1=mysqli_query($db,$pl);
+
+     if(mysqli_num_rows($us1)==1 and mysqli_num_rows($pl1)==1){
+             $que="SELECT ludo_id from running where player1='$user'";
+             $res=mysqli_query($db,$que);
+             $query1="SELECT player1,player2 from running where player1='$user'";
+             $res1=mysqli_query($db,$query1);
+             $query1="SELECT amount from running where player1='$user'";
+             $res2=mysqli_query($db,$query1);
+             $queu="SELECT room_id from running where player1='$user'";
+             $res3=mysqli_query($db,$queu);
+           }
+           else{
+               $que="SELECT ludo_id from running where player2='$user'";
+               $res=mysqli_query($db,$que);
+               $query1="SELECT player1,player2 from running where player2='$user'";
+               $res1=mysqli_query($db,$query1);
+               $query1="SELECT amount from running where player2='$user'";
+               $res2=mysqli_query($db,$query1);
+               $queu="SELECT room_id from running where player2='$user'";
+               $res3=mysqli_query($db,$queu);
+           }
+
+       
+?>
 <?php include('includes/header.php');?>
 <?php include('includes/nav.php');?>
-<?php include('includes/server.php');?>
-
 <br>
 <br>
 <br>
-<?php 
-     $user=$_SESSION['user_name'];
-     $ludo_id=$_POST['postid'];
-     $que="SELECT ludo_id from running where ludo_id='$ludo_id'";
-     $res=mysqli_query($db,$que);
-     $query1="SELECT player2 from running where ludo_id='$ludo_id'";
-     $res1=mysqli_query($db,$query1);
-     $query1="SELECT amount from running where ludo_id='$ludo_id'";
-     $res2=mysqli_query($db,$query1);
-     $queu="SELECT room_id from running where ludo_id='$ludo_id'";
-     $res3=mysqli_query($db,$queu);
-
-?>
 <div class="container"><h4>Ludo Match ID:
     <?php 
         
@@ -30,7 +47,9 @@
        <h2 class="text-center"><?php echo $_SESSION['user_name']; ?> vs <?php 
           if (mysqli_num_rows($res1) == 1){
            while ($row=mysqli_fetch_assoc($res1)) {
-              echo $row['player2'];}}?></h2>
+              if($row['player1']==$user){echo $row['player2'];}
+              else {echo $row['player1'];}
+              }}?></h2>
 
        <h4 class="text-center">
         <?php 
