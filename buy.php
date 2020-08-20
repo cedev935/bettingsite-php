c42ec1b94127fd0e15abc72d0890c1d236b69b09
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-20 18:21:19 +0530

    buy page updated

--- buy.php
+++ buy.php
@@ -1,3 +1,9 @@
+<?php
+    header("Pragma: no-cache");
+    header("Cache-Control: no-cache");
+    header("Expires: 0");
+
+?>
 <?php include('includes/header.php');?>
 <?php include('includes/nav.php');?>
 <br>
@@ -52,6 +58,56 @@
     </form>
 
 </div>
+
+<form method="post" action="pgRedirect.php">
+        <table border="1" class=" m-5 ">
+            <tbody>
+                <tr>
+                    <th>S.No</th>
+                    <th>Label</th>
+                    <th>Value</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><label>ORDER_ID::*</label></td>
+                    <td><input id="ORDER_ID" tabindex="1" maxlength="20" size="20"
+                        name="ORDER_ID" autocomplete="off"
+                        value="<?php echo  "ORDS" . rand(10000,99999999)?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td><label>CUSTID ::*</label></td>
+                    <td><input id="CUST_ID" tabindex="2" maxlength="12" size="12" name="CUST_ID" autocomplete="off" value="CUST001"></td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td><label>INDUSTRY_TYPE_ID ::*</label></td>
+                    <td><input id="INDUSTRY_TYPE_ID" tabindex="4" maxlength="12" size="12" name="INDUSTRY_TYPE_ID" autocomplete="off" value="Retail"></td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td><label>Channel ::*</label></td>
+                    <td><input id="CHANNEL_ID" tabindex="4" maxlength="12"
+                        size="12" name="CHANNEL_ID" autocomplete="off" value="WEB">
+                    </td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td><label>txnAmount*</label></td>
+                    <td><input title="TXN_AMOUNT" tabindex="10"
+                        type="text" name="TXN_AMOUNT"
+                        value="1">
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td><input value="CheckOut" type="submit"   onclick=""></td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
 <hr>
 <br>
 <br>
