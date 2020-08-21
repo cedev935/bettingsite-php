24e84ae652fbed9f20a57699dd553ddcb8c16c16
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-21 15:09:19 +0530

    Update login_validation.php

--- includes/login_validation.php
+++ includes/login_validation.php
@@ -1,6 +1,6 @@
 <?php
 	if(!empty($_SESSION['userLogin'])=='true'){
-		die("You must login first, Go to http://localhost/online_outpass_portal/login.php to login");
+		die("You must login first, Go to http://www.ludosociety.com/login.php to login");
 	    //header("Location:sample_login.php");
 	}
 ?>
