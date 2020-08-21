c408183d8bbe66163a70afbaa0c4cc342d3b8d56
cedev935 <cedev935@gmail.com>
2020-08-21 14:57:41 +0530

    Razorpay Payment Gatway added successfully

---
+++ razor/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$keyId = 'rzp_live_8OvRcoBNwUZMGa';
+$keySecret = 'yjDAVdd5DbJDOxZtFnWRU9i3';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
