745428302d9f52b22869d890750202dde0cadf20
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-22 18:28:39 +0530

    updated

--- uploads/upload.php
+++ uploads/upload.php
@@ -2,20 +2,53 @@
 <?php
 $radioVal = $_POST["match_status"];
 $ludo_id = $_POST['ludo_id'];
+$ph=$_SESSION['phone_no'];
+$user=$_SESSION['user_name'];
 
 if($radioVal == "won")
 {
     echo("You won");
-    
+    $sql="UPDATE history set winner='$ph' where ludo_id='$ludo_id'";
+    mysqli_query($db,$sql);
+    $sql="SELECT * from history where ludo_id='$ludo_id'";
+    $res=mysqli_query($db,$sql);
+    if(mysqli_num_rows($res)==1){
+      while($row=mysqli_fetch_assoc($res)){
+          $winner=$row['winner'];
+          $looser=$row['looser'];
+          if($winner!='NULL' and $looser!='NULL'){
+              $query2="DELETE from running where player1='$user' or player2='$user'";
+              mysqli_query($db,$query2); 
+          }
+      }
+   }
     $target_dir = "won/";
 }
 else if ($radioVal == "loose")
 {
     echo("You lost");
+    $sql="UPDATE history set looser='$ph' where ludo_id='$ludo_id'";
+    mysqli_query($db,$sql);
+    $sql="SELECT winner,looser from history where ludo_id='$ludo_id'";
+    $res=mysqli_query($db,$sql);
+    if(mysqli_num_rows($res)==1){
+      while($row=mysqli_fetch_assoc($res)){
+          $winner=$row['winner'];
+          $looser=$row['looser'];
+          if($winner!='NULL' and $looser!='NULL'){
+              $query2="DELETE from running where player1='$user' or player2='$user'";
+              mysqli_query($db,$query2); 
+          }
+      }
+   }
     $target_dir = "loose/";
 }
 else{
     echo("cancel");
+    $query2="DELETE from running where player1='$user' or player2='$user'";
+    mysqli_query($db,$query2);
+    $query1="DELETE from history where ludo_id='$ludo_id' or winner='$ph' or looser='$ph'";
+    mysqli_query($db,$query1);
     $target_dir = "cancel/";
 }
 
@@ -69,24 +102,6 @@ else {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     echo "<script>alert('Uploaded')</script>";
 
-     $ph=$_SESSION['phone_no'];
-    $user=$_SESSION['user_name'];
-    
-     if ($radioVal =='won'){
-        $sql="UPDATE history set winner='$ph' where ludo_id='$ludo_id'";
-        mysqli_query($db,$sql);      
-      }
-  if ($radioVal =='loose'){
-        $sql="UPDATE history set looser='$ph' where ludo_id='$ludo_id'";
-        mysqli_query($db,$sql);      
-      }
-    if ($radioVal =='cancel'){
-        $query2="DELETE from running where player1='$user' or player2='$user'";
-        mysqli_query($db,$query2);
-        $query1="DELETE from history where ludo_id='$ludo_id' or winner='$ph' or looser='$ph'";
-        mysqli_query($db,$query1); 
-      }
-
 
    $sql="SELECT winner,looser from history where ludo_id='$ludo_id'";
    $res=mysqli_query($db,$sql);
@@ -97,6 +112,7 @@ else {
           if($winner!='NULL' and $looser!='NULL'){
               $query2="DELETE from running where player1='$user' or player2='$user'";
               mysqli_query($db,$query2); 
+              header('location:home.php');
           }
       }
    }
