9ea2a809e2ba5ee1af2cc2700e92515b2d973124
cedev935 <cedev935@gmail.com>
2020-08-22 19:28:52 +0530

    auto redirect tto transaction page after successful payment with chips increasing

--- razor/verify.php
+++ razor/verify.php
@@ -40,12 +40,18 @@ if ($success === true)
 {
     $html = "<p>Your payment was successful</p>
              <p>Payment ID: {$_POST['razorpay_payment_id']}</p>";
+    
         
          $db = mysqli_connect('localhost', 'root', '', 'ludo_legion');  
          $amount=$_SESSION['amount'];
          $number=$_SESSION['number'];
          $sql="UPDATE chips set chips=chips+$amount where user='$number'";
          mysqli_query($db,$sql);
+    
+        echo $amount;
+        echo $number;
+        sleep(3);
+        header('location: ../transaction.php');
 }
 else
 {
