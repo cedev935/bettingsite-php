af5cadf7d231e45c0d4524c5d0b4977722d42353
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-22 22:21:50 +0530

    chips update
    10% commission update

--- admin/admin_server1.php
+++ admin/admin_server1.php
@@ -6,13 +6,16 @@
 	$amount=$_POST['postamount'];
 	$lphone_no=$_POST['postphone1'];
 	
-	$reject1 = "UPDATE chips set chips=chips+$amount WHERE user='$wphone_no'";
-	mysqli_query($db, $reject1);
+	//caluculate 10% from amount
+	$remove=floor($amount*0.1);
+	//remove 10 % from final amount
+	$amount=$amount-$remove;
 
-	$reject1 = "UPDATE chips set chips=chips-$amount WHERE user='$lphone_no'";
+	$reject1 = "UPDATE chips set chips=chips+$amount WHERE user='$wphone_no'";
 	mysqli_query($db, $reject1);
 	
     $reject = "DELETE FROM history WHERE looser='$lphone_no'";
     mysqli_query($db, $reject);
+   	
     
 ?>
