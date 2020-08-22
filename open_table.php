340ed87bb3ee3b829d57f46c7bfdb60403046020
Madhu <67431480+Madh0906@users.noreply.github.com>
2020-08-22 10:05:02 +0530

    uppdate

--- open_table.php
+++ open_table.php
@@ -29,6 +29,7 @@
                 <!--                      If user wants to accept a game set by others-->
 
                 <td><button type="button" onclick="post(<?php  echo $r['phone_no']; ?>);" class="btn btn-sm btn-success">Play</button></td>
+                <td><input type="text" id="room_id" placeholder="Enter room id "></td>
                 <?php } ?>
 
 
