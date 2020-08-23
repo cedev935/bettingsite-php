d0c8c10a058ef0955f63f9033b382ebe381eb8cf
cedev935 <cedev935@gmail.com>
2020-08-23 12:45:08 +0530

    Instruction page added with server.php included

---
+++ instructions.php
@@ -0,0 +1,27 @@
+<?php include('includes/server.php');?>
+<?php include('includes/header.php');?>
+<?php include('includes/nav.php');?>
+
+<br>
+<br>
+<br>
+
+<div class="container">
+    <h4>Instructions</h4>
+    <hr>
+    <p>
+    1.You can set a game for yourself or you can chooose to play a game set by others<br><br>
+    2.Once you click on play button, You need to create a roomid on ludoking app and enter it when asked
+    in prompt box<br><br>
+    3.After that you and your oppponent will be redirected to room code page<br><br>
+    4.Your opponent will join the game using room code set by you or vice versa if he clicks play button<br><br>
+    5.Finally play the game and upload your winning or loosing screenshot in room page<br><br>
+    6.After that admin will approve winners and tokens will be added to winners after deducting platform charges, which you can withdraw by placing  withdrawal requests<br><br>
+    </p>
+    
+</div>
+<br>
+<br>
+<br>
+
+<?php include('includes/bottom_nav.php');?>
